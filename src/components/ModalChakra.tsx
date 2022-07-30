9227db9a41a873eb86f57aa45e79c2265efc0d78
MarioLeandro <marioleandro34@gmail.com>
2022-07-30 08:40:56 -0300

    feat: add scroll in ModalChakra

--- src/components/ModalChakra.tsx
+++ src/components/ModalChakra.tsx
@@ -1,5 +1,15 @@
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from "@chakra-ui/react"
-import instructions from "../utils/instructions.json"
+import {
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import instructions from "../utils/instructions.json";
 
 import {
   Table,
@@ -11,60 +21,64 @@ import {
   Td,
   TableCaption,
   TableContainer,
-  useColorModeValue
-} from '@chakra-ui/react'
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 function ModalChakra() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    
-    return (
-      <>
-        <Button 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button
         mr={4}
         mt={4}
         bg={"inherit"}
         variant="outline"
         borderColor={"gray.600"}
-        onClick={onOpen}>Instruções</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose} size={"xl"} >
-          <ModalOverlay />
-          <ModalContent minW='1000px' minH='600px' borderRadius="10">
-            <ModalHeader borderRadius="10">Instruções MIPS</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <TableContainer>
-                <Table
-                  size={"lg"} 
-                  variant='striped'
-                  colorScheme= "green">
-                  <Thead>
+        onClick={onOpen}
+      >
+        Instruções
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size={"xl"}
+        scrollBehavior={"inside"}
+      >
+        <ModalOverlay />
+        <ModalContent minW="1000px" minH="600px" borderRadius="10">
+          <ModalHeader borderRadius="10">Instruções MIPS</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <TableContainer>
+              <Table size={"md"} variant="striped" colorScheme="green">
+                <Thead>
+                  <Tr>
+                    <Th>Instrução</Th>
+                    <Th>Tipo</Th>
+                    <Th>Opcode</Th>
+                    <Th>Função</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {instructions.map((instruction) => (
                     <Tr>
-                      <Th>Instrução</Th>
-                      <Th>Tipo</Th>
-                      <Th>Opcode</Th>
-                      <Th>Função</Th>
+                      <Td>{instruction.instruction}</Td>
+                      <Td>{instruction.type}</Td>
+                      <Td>{instruction.opcode}</Td>
+                      <Td>{instruction.function ? instruction.function : 0}</Td>
                     </Tr>
-                  </Thead>
-                  <Tbody>
-                    {
-                      instructions.map((instruction) => (
-                        <Tr>
-                          <Td>{instruction.instruction}</Td>
-                          <Td>{instruction.type}</Td>
-                          <Td>{instruction.opcode}</Td>
-                          <Td>{instruction.function ? instruction.function : 0}</Td>
-                        </Tr>
-                      ))
-                    }
-                  </Tbody>
-                </Table>
-              </TableContainer>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </>
-    ) 
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default ModalChakra;
